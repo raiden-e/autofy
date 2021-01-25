3e7883da2c720eb430ec1dc6832168d954cfcd1a
Omglolyes <raiden.erdmann@gmail.com>
2021-01-25 12:38:03 +0100

    update formatting LoFi

--- LoFi.py
+++ LoFi.py
@@ -10,16 +10,42 @@ _spotify = get_spotify_client()
 
 
 def deduplify_list(potential_duplicates: list, reference_deuplicates: list):
+    def print_diff(a, b):
+        print("Duplicate Meta:\n {:>21}|{:<0}".format(
+            a['track']['name'], b['track']['name']))
+        print("{:>32}|{:<0}".format(
+            a['track']['id'], b['track']['id']))
+        print("{:>32}|{:<0}".format(
+            a['track']['artists'][0]['name'], b['track']['artists'][0]['name']))
+
     for x in reference_deuplicates:
         for y in potential_duplicates:
             if x["track"]["id"] == y["track"]["id"]:
+                print("Duplicate ID: {0:30}- {1}".format(
+                    y['track']['name'], f"{x['track']['id']}|{y['track']['id']}"))
                 potential_duplicates.remove(y)
+                continue
+            # if duration somewhat same, artist and track name same
+            if y["track"]["duration_ms"] - 100 <= x["track"]["duration_ms"] <= y["track"]["duration_ms"] + 100:
+                if x["track"]["name"] == y["track"]["name"]:
+                    if len(x["track"]["artists"]) == 1:
+                        if x["track"]["artists"][0] in y["track"]["artists"]:
+                            print_diff(x, y)
+                            potential_duplicates.remove(y)
+                            continue
+                    else:
+                        for artist in x["track"]["artists"]:
+                            if artist['name'] in y["track"]["artists"]:
+                                print_diff(x, y)
+                                potential_duplicates.remove(y)
+                                continue
+
     return potential_duplicates
 
 
 def randomize_lofi(initial_track: dict, lofi_base: list, lofi_list: list):
     try:
-        list_size = 250 - 1 # We count -1 bc of initial track
+        list_size = 250 - 1  # We count -1 bc of initial track
         list_sample = random.sample(lofi_list, (list_size - len(lofi_base)))
         final_sample = random.sample([*lofi_base, *list_sample], list_size)
         return [z["track"]["id"] for z in [initial_track, *final_sample]]
