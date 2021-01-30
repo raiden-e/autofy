55ebc4c49f1e02dfca389e98d1bf2dc90078ac99
Omglolyes <raiden.erdmann@gmail.com>
2021-01-30 23:09:46 +0100

    add JPN to lofi

--- LoFi.py
+++ LoFi.py
@@ -5,7 +5,18 @@ from util import playlist
 from util.spotify import get_spotify_client
 
 username = "raiden_e"
-lofi_id = "5h9LqGUUE4FKQfVwgAu1OA"
+lofi = {
+    "name": "lofi",
+    "id": "5h9LqGUUE4FKQfVwgAu1OA",
+    "backup":"31k9ZXIfUi9v5mpbfg6FQH",
+    "base":"5adSO5spsp0tb48t2MyoD6",
+}
+japan = {
+    "name": "japan",
+    "id": "6Cu6fL6djm63Em0i93IRUW",
+    "backup":"19yEs2hB6J5FwHrGMsIlZQ",
+    "base":"7EdczIpYSY7aUBSMqgGguV",
+    }
 _spotify = get_spotify_client()
 
 
@@ -19,17 +30,17 @@ def randomize_lofi(initial_track: dict, lofi_base: list, lofi_list: list):
         raise "Could not sample lofibase or lofilist"
 
 
-def main():
-    if playlist.edited_this_week(_spotify, lofi_id):
+def main(id: str, backup: str, base: str):
+    if playlist.edited_this_week(_spotify, id):
         print("Exiting, Ran this week")
         return
 
     print("getting playlist Backup")
     lofi_list = playlist.getAsync(
-        _spotify, "31k9ZXIfUi9v5mpbfg6FQH", True)["items"]
+        _spotify, backup, True)["items"]
     print("getting playlist base")
     lofi_base = playlist.get(
-        _spotify, "5adSO5spsp0tb48t2MyoD6", True)['items']
+        _spotify, base, True)['items']
 
     print("deduplifying list")
     lofi_list = playlist.deduplify_list(lofi_list, lofi_base)
@@ -42,15 +53,17 @@ def main():
     weekly_playlistIds = randomize_lofi(initial_track, lofi_base, lofi_list)
 
     print("clearing playlist")
-    playlist.clear(_spotify, lofi_id)
+    playlist.clear(_spotify, id)
 
     print("adding songs to playlist")
     playlist.add(
         _spotify=_spotify,
         tracks_to_add=weekly_playlistIds,
-        playlistId=lofi_id
+        playlistId=id
     )
 
 
 if __name__ == '__main__':
-    main()
+    for x in (lofi, japan):
+        print(f'shuffeling {x["name"]}')
+        main(x['id'], x['backup'], x['base'])
