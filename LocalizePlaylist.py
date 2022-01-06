53724d6c4746d69eba6fed26627fde16cb1798ec
Raiden Erdmann <raiden.erdmann@gmail.com>
2022-01-06 16:55:00 +0100

    finding alg workting

--- LocalizePlaylist.py
+++ LocalizePlaylist.py
@@ -2,32 +2,44 @@ import json
 import os
 import re
 import sys
+# import spotipy
+# import pprint
 
 
 def main(folder) -> None:
+    def escape(inp: str):
+        for char in ('.', ' ', '(', ')', '[', ']', '{', '}', '^', '$', '+'):
+            inp = inp.replace(char, '\\' + char)
+        for char in ('"', "'", "├»"):
+            inp = inp.replace(char, ".?")
+        return inp
+
     def find_relative(tr, folder):
-        re_match = f"({tr['artists'][0]['name']}"
-        if len(tr['artists'] > 1):
+        # match at least one artist
+        re_match = f".?({escape(tr['artists'][0]['name'])}"
+        if len(tr['artists']) > 1:
             for artist in tr['artists'][1:]:
-                re_match += f"|{artist['name']}"
+                re_match += f"|{escape(artist['name'])}"
 
-        re_match += ")+.*.*-"
-        re_match += str(tr['name'])
+        # add a "-" symbol
+        re_match += ")+.?\\ *-\\ *.?"
+        re_match += escape(tr['name'])
+        re_match += ".?"
 
-        for file in os.walk(folder):
-
-        if re.match(f"{tr}.mp3", file):
-            return file.fullname
+        for root, _, files in os.walk(folder):
+            # pprint.pp((root, dirs, files))
+            print(root)
+            for file in files:
+                if file[-4:] in ('.mp3', '.m4a', 'flac', '.wav', '.ogg'):
+                    if re.match(re_match, file):
+                        return os.path.join(f"{root}\\{file}")
         return False
-
-    def get_deep_search(track):
-        print(track)
 
     def export(pl, folder, playlist_file):
         output = "#EXTM3U\n"
         for tr in pl:
-            output += f"#EXTINF:{tr.no}, {tr.artist} - {tr.title}\n"
-            output += str(tr.path.trim(folder))
+            output += f"#EXTINF:{int(tr[1]['duration_ms']/1000)}, {tr[1]['artists'][0]['name']} - {tr[1]['name']}\n"
+            output += "." + str(tr[0]).replace(folder, "") + "\n"
 
         with open(playlist_file, 'w', encoding='utf-8') as f:
             f.write(output)
@@ -36,17 +48,12 @@ def main(folder) -> None:
         data = json.load(f)
 
     tracks = data['tracks']['items']
-    playlist, deep_search = [], []
+    playlist = []
     for track in tracks:
+        print(f"=== Searching {track['track']['name']} ===")
         rel_dir = find_relative(track['track'], folder)
         if rel_dir:
-            playlist.append(rel_dir)
-        else:
-            deep_search.append(track)
-
-        if len(deep_search) > 0:
-            for tr in deep_search:
-                get_deep_search(tr)
+            playlist.append([rel_dir, track['track']])
 
     export(playlist, folder, os.path.join(folder, "new_playlist.m3u8"))
 
