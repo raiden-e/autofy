9aba42ad9245467d3d36103b0a3d487d15701564
Raiden Erdmann <raiden.erdmann@gmail.com>
2026-07-19 16:33:31 +0200

    jsonc comments in the gist

--- util/gist.py
+++ util/gist.py
@@ -1,5 +1,6 @@
 import json
 
+import json5
 import config
 from github import Github, InputFileContent
 
@@ -9,9 +10,20 @@ token = config.GIST['TOKEN']
 gist = Github(token).get_gist(gist_id)
 
 
+def _fix_surrogates(obj):
+    # Fixes escaped UTF-16 surrogate pairs, like emojis
+    if isinstance(obj, str):
+        return obj.encode("utf-16", "surrogatepass").decode("utf-16")
+    if isinstance(obj, dict):
+        return {_fix_surrogates(k): _fix_surrogates(v) for k, v in obj.items()}
+    if isinstance(obj, list):
+        return [_fix_surrogates(v) for v in obj]
+    return obj
+
+
 def load(gist_name):
     x = gist.files[gist_name].content
-    return json.loads(x)
+    return _fix_surrogates(json5.loads(x))
 
 
 def update(filename: str, content, description: str):
